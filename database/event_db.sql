997b6da2bb0a2a1c35020304d8690cc2bed36b32
JahanRIya <riyascookingvlog@gmail.com>
2023-06-19 23:13:02 +0600

    first commit

---
+++ database/event_db.sql
@@ -0,0 +1,259 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 21, 2023 at 08:23 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `event_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `audience`
+--
+
+CREATE TABLE `audience` (
+  `id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `contact` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `address` text NOT NULL,
+  `event_id` int(30) NOT NULL,
+  `payment_status` tinyint(1) NOT NULL DEFAULT 0 COMMENT '0= pending, 1 =Paid',
+  `attendance_status` tinyint(1) NOT NULL DEFAULT 0 COMMENT '1= present',
+  `status` tinyint(1) NOT NULL DEFAULT 0 COMMENT '0 = for verification,  1 = confirmed,2= declined',
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `audience`
+--
+
+INSERT INTO `audience` (`id`, `name`, `contact`, `email`, `address`, `event_id`, `payment_status`, `attendance_status`, `status`, `date_created`) VALUES
+(2, 'Jahan', '01889313273', 'jahan@gmail.com', 'Jamalpur Sadar', 1, 1, 0, 1, '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+
+CREATE TABLE `events` (
+  `id` int(30) NOT NULL,
+  `venue_id` int(30) NOT NULL,
+  `event` text NOT NULL,
+  `description` text NOT NULL,
+  `schedule` datetime NOT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT 1 COMMENT '1=Public, 2-Private',
+  `audience_capacity` int(30) NOT NULL,
+  `payment_type` tinyint(1) NOT NULL DEFAULT 1 COMMENT '1=Free,payable',
+  `amount` double NOT NULL DEFAULT 0,
+  `banner` text NOT NULL,
+  `date_created` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `events`
+--
+
+INSERT INTO `events` (`id`, `venue_id`, `event`, `description`, `schedule`, `type`, `audience_capacity`, `payment_type`, `amount`, `banner`, `date_created`) VALUES
+(1, 1, 'Wedding', '&lt;p&gt;&lt;/p&gt;&lt;h2 style=&quot;-webkit-tap-highlight-color: rgba(0, 0, 0, 0); margin-top: 1.5em; margin-bottom: 1.5em; line-height: 1.5; animation: 1000ms linear 0s 1 normal none running fadeInLorem;&quot;&gt;&lt;b style=&quot;-webkit-tap-highlight-color: rgba(0, 0, 0, 0); margin-top: 1.5em; margin-bottom: 1.5em; line-height: 1.5; animation: 1000ms linear 0s 1 normal none running fadeInLorem;&quot;&gt;Happy Wedding &lt;/b&gt;&lt;/h2&gt;&lt;p style=&quot;-webkit-tap-highlight-color: rgba(0, 0, 0, 0); margin-top: 1.5em; margin-bottom: 1.5em; line-height: 1.5; animation: 1000ms linear 0s 1 normal none running fadeInLorem;&quot;&gt;&lt;b style=&quot;-webkit-tap-highlight-color: rgba(0, 0, 0, 0); margin-top: 1.5em; margin-bottom: 1.5em; line-height: 1.5; animation: 1000ms linear 0s 1 normal none running fadeInLorem;&quot;&gt;“A successful marriage requires falling in love many times, always with the same person" ', '2023-05-20 05:00:00', 1, 500, 2, 500, 'wedding.jpg', '0000-00-00 00:00:00'),
+(2, 2, 'Birthday', 'Count your age by friends, not years. Count your life by smiles, not tears. — John Lennon ', '2023-05-01 18:00:00', 1, 500, 1, 0, 'birth.png', '0000-00-00 00:00:00'),
+(3, 2, 'Marriage Anniversery', 'Happy Anniversery', '2023-04-28 17:00:00', 2, 300, 1, 0, 'marriage anniversery.jpg', '0000-00-00 00:00:00'),
+(4, 2, 'Rice Ceremory', 'Happy Ceremony', '2023-04-23 00:21:00', 1, 100, 1, 0, 'rice ceremony.png', '0000-00-00 00:00:00'),
+(5, 1, 'Mehedy Program', 'Happy Mehedy Night', '2023-05-20 00:21:00', 1, 100, 1, 0, 'mehedy.jpg', '0000-00-00 00:00:00'),
+(6, 3, 'Seminar/ Meeting', 'Happy Meeting', '2023-05-23 00:21:00', 3, 100, 1, 0, 'meeting.jpg', '0000-00-00 00:00:00');
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_settings`
+--
+
+CREATE TABLE `system_settings` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `cover_img` text NOT NULL,
+  `about_content` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `system_settings`
+--
+
+INSERT INTO `system_settings` (`id`, `name`, `email`, `contact`, `cover_img`, `about_content`) VALUES
+(1, 'Dream Event', 'dream.info@gmail.com', '018893132735', 'cover.jpg', '&lt;p style=&quot;text-align: center; background: transparent; position: relative;&quot;&gt;&lt;span style=&quot;color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; font-weight: 400; text-align: justify;&quot;&gt;&amp;nbsp; Dream Event is a multi-benefit imaginative office based close to Connecticut. We make, structure, and convey events of every kind all through the US and around the world. We have been putting on unprecedented events for more than 25 years, and amid that time, we have been quite great at it! We have fabricated a committed group of specialists from the universe of Event Management and AV, which can bring your vision into the real world. Regardless of whether you are a major national or universal company with an extensive spending plan or a private venture with constrained money-related assets, we have the energy, ability, and experience to make your event a novel and noteworthy event that you will think back to proudly..&lt;/span&gt;&lt;br&gt;&lt;/p&gt;&lt;p style=&quot;text-align: center; background: transparent; position: relative;&quot;&gt;&lt;br&gt;&lt;/p&gt;&lt;p style=&quot;text-align: center; background: transparent; position: relative;&quot;&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;&lt;/p&gt;');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `username` varchar(200) NOT NULL,
+  `password` text NOT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT 2 COMMENT '1=Admin,2=Staff'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `username`, `password`, `type`) VALUES
+(1, 'Administrator', 'admin', '0192023a7bbd73250516f069df18b500', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `venue`
+--
+
+CREATE TABLE `venue` (
+  `id` int(30) NOT NULL,
+  `venue` text NOT NULL,
+  `address` text NOT NULL,
+  `description` text NOT NULL,
+  `rate` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `venue`
+--
+
+INSERT INTO `venue` (`id`, `venue`, `address`, `description`, `rate`) VALUES
+(1, 'Luise Village', 'Jamalpur Sadar', 'Rent Category : Rent for Social Program', 3000),
+(2, 'Star Community Centre', 'Jamalpur Sadar', 'Rent Category : Rent for Social Program', 2500),
+(3, 'Mirja Ajom Auditorium', 'Jamalpur Sadar', 'Rent Category : Rent for Meeting/Seminar', 2800),
+(4, 'Bijoy 71 Hall', 'Dhaka', 'Rent Category : Rent for Meeting/Seminar', 1000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `venue_booking`
+--
+
+CREATE TABLE `venue_booking` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `address` text NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `contact` varchar(100) NOT NULL,
+  `venue_id` int(30) NOT NULL,
+  `duration` varchar(100) NOT NULL,
+  `datetime` datetime NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 0 COMMENT '0-for verification,1=confirmed,2=canceled'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `venue_booking`
+--
+
+INSERT INTO `venue_booking` (`id`, `name`, `address`, `email`, `contact`, `venue_id`, `duration`, `datetime`, `status`) VALUES
+(1, 'Jahan Riya', 'Mirza Ajom Auditorium', 'riya@gmail.com', '018893132735', 2, '1 night', '2020-10-14 17:00:00', 1),
+(2, 'Anwar', 'Luise Village , Dhoripara , Jamalpur Sadar.', 'anwar@gmail.com', '018893132736', 4, '2', '2023-04-15 00:18:00', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `audience`
+--
+ALTER TABLE `audience`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `venue`
+--
+ALTER TABLE `venue`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `venue_booking`
+--
+ALTER TABLE `venue_booking`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `audience`
+--
+ALTER TABLE `audience`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `events`
+--
+ALTER TABLE `events`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `venue`
+--
+ALTER TABLE `venue`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `venue_booking`
+--
+ALTER TABLE `venue_booking`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
